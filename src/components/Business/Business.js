4bafe1a0f5592a9c9d7bb3cead12cb97ea3148b4
johnfarmernyc <johnfarmernyc@gmail.com>
2019-02-13 22:18:38 -0500

    fixed syntax issue with lowercased React.component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -13,28 +13,27 @@ const business = {
   reviewCount: 90
 };
 
-
-class Business extends React.component {
-	render () {
+class Business extends React.Component {
+	render(){
 		return (
-				<div className="Business">
-	  				<div className="image-container">
-	   					<img src={business.imageSrc} alt=''/>
-	 				</div>
-	  				<h2>{business.name}</h2>
-	  				<div className="Business-information">
-	    			<div className="Business-address">
-			      		<p>{business.address}</p>
-			      		<p>{business.city}</p>
-			      		<p>{business.state} {business.zipCode}</p>
-			    	</div>
-			    	<div className="Business-reviews">
-			      		<h3>{business.category}</h3>
-			      		<h3 class="rating">{business.rating}</h3>
-			      		<p>{business.reviewCount}</p>
-			    	</div>
-		 			</div>
-				</div>
+      <div className="Business">
+        <div className="image-container">
+          <img src={business.imageSrc} alt=''/>
+        </div>
+        <h2>{business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{business.address}</p>
+            <p>{business.city}</p>
+            <p>{business.state} {business.zipCode}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{business.category}</h3>
+            <h3 class="rating">{business.rating}</h3>
+            <p>{business.reviewCount}</p>
+          </div>
+        </div>
+      </div>
 			)
 	}
 };
